2f1a7fb741ce0a1018b8594fe337609373ba02a3
lukedinkler <ljdinkler@gmail.com>
2015-07-16 19:54:23 -0400

    Additions to funclib

--- App/funclib.py
+++ App/funclib.py
@@ -8,5 +8,11 @@ def AdminEx(command):
     win32shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c ' + command)
 
 def DisableGuest():
-    AdminExe("net user guest /active:no >NUL")
+    AdminEx("net user guest /active:no >NUL")
 
+def EnableFirewall():
+    AdminEx("netsh advfirewall set allprofiles state on")
+
+def EnableUAC():
+    AdminEx("C:\Windows\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 2 /f")
+    #Requires reboot
