fbafd2cde56b1d6207690c609a5e8c9b55d35c9b
lukedinkler <ljdinkler@gmail.com>
2015-07-16 19:43:45 -0400

    ...

--- App/funclib.py
+++ App/funclib.py
@@ -4,3 +4,5 @@ import os, win32
 import win32com.shell.shell as win32shell
 import winshell
 
+def AdminEx(command):
+    win32shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c ' + command)
