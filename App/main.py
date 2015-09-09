943753882a03927fafcaf6b3b4c1decc848cb8ea
Luke Dinkler <ljdinkler@gmail.com>
2015-09-09 15:54:37 -0400

    Added About tab on main menu

--- App/main.py
+++ App/main.py
@@ -2,9 +2,14 @@
 import GUI, funclib
 
 def Init():
-        MainMenu = GUI.buttonbox("Welcome to SecureMe! Please select an option to begin", choices=["Full Secure", "Partial Secure", "About"])
+        MainMenu()
         
-        
-
+def MainMenu():
+       menu = GUI.buttonbox("Welcome to SecureMe! Please select an option to begin", choices=["Full Secure", "Partial Secure", "About"])
+       if menu == "About":
+               about = GUI.buttonbox("""SecureMe Version 0.1 Beta 0
+Developed by Luke Dinkler and Peter Toth 2015""", choices=["OK"])
+               MainMenu()
+               
 if __name__ == '__main__':
         Init()
