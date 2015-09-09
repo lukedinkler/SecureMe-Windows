89af8997e0fcc0b4440459be22b1d3ff8e8f4705
Luke Dinkler <ljdinkler@gmail.com>
2015-09-09 16:06:30 -0400

    Added 1st draft SecureMe icon to main menu

--- App/main.py
+++ App/main.py
@@ -5,10 +5,10 @@ def Init():
         MainMenu()
         
 def MainMenu():
-       menu = GUI.buttonbox("Welcome to SecureMe! Please select an option to begin", choices=["Full Secure", "Partial Secure", "About"])
+       menu = GUI.buttonbox("Welcome to SecureMe! Please select an option to begin", choices=["Full Secure", "Partial Secure", "About"], image="data/SecureMe-icon-d1.gif")
        if menu == "About":
                about = GUI.buttonbox("""SecureMe Version 0.1 Beta 0
-Developed by Luke Dinkler and Peter Toth 2015""", choices=["OK"])
+Developed by Luke Dinkler and Peter Toth 2015""", choices=["Back"], image="data/about-icon.gif")
                MainMenu()
                
 if __name__ == '__main__':
