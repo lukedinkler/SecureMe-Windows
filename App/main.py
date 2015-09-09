118ce368b9aaeb429bdcbd50197b58f866d76bdb
Luke Dinkler <ljdinkler@gmail.com>
2015-09-09 16:40:38 -0400

    Incorporated Password Policies into main.py

--- App/main.py
+++ App/main.py
@@ -11,18 +11,22 @@ def MainMenu():
 Developed by Luke Dinkler and Peter Toth 2015""", choices=["Back"], image="data/about-icon.gif")
                MainMenu()
        elif menu == "Choose Security Option":
-               securitypieces = GUI.choicebox("Choose a system security action to perform:", choices=["Firewall: Enable", "Firewall: Disable"])
+               securitypieces = GUI.choicebox("Choose a system security action to perform:", choices=["Firewall: Enable", "Firewall: Disable", "Set Password Policies"])
                if securitypieces == None:
                        MainMenu()
                elif securitypieces == "Firewall: Enable":
                         funclib.EnableFirewall()
-                        GUI.msgbox("Active successfully performed!")
+                        GUI.msgbox("Action successfully performed!")
                         MainMenu()
+               elif securitypieces == "Set Password Policies":
+                       funclib.SetPasswordPolicies()
+                       GUI.msgbox("Action successfully performed!")
+                       MainMenu()
                 
        elif menu == "Full Lockdown":
                 funclib.DisableGuest()
                 funclib.EnableFirewall()
-
+                funclib.SetPasswordPolicies()
                 
                 GUI.buttonbox("SecureMe has fully locked down and secured your computer!", choices=["OK!"], image="data/check.gif")
                 MainMenu()
