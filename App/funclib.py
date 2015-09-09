2c86dffc40b85ef660728cb7dda398f313acd0e3
Luke Dinkler <ljdinkler@gmail.com>
2015-09-09 15:48:50 -0400

    Got set up with temp. GUI library

--- App/funclib.py
+++ App/funclib.py
@@ -2,7 +2,6 @@
 #Function Library for SecureMe
 import os, win32
 import win32com.shell.shell as win32shell
-import winshell
 
 def AdminEx(command):
     win32shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c ' + command)
