081236fb452e77e37cf035b1379baea34a907704
Luke Dinkler <ljdinkler@gmail.com>
2016-02-19 20:03:51 -0500

    Graphical garbage

--- App/SecureMe/SecureMe/obj/Release/SecureMe_Content.g.i.cs
+++ App/SecureMe/SecureMe/obj/Release/SecureMe_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/folder-icon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/package-icon.png")]
 
 
